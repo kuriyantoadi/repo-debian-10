f5524a580760571054c56a153be6cffc763cf8eb
Kuriyanto Adi Saputro <melatikuning212@gmail.com>
2023-09-28 06:35:47 +0700

    Create repo.sh

---
+++ repo.sh
@@ -0,0 +1,3 @@
+wget https://github.com/kuriyantoadi/repo-debian-10/raw/master/contrib-non-free
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bc
+cat contrib-non-free > /etc/apt/sources.list /etc/apt/sources.list
